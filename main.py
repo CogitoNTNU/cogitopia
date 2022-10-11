85a5f9fc23f64f48ad7fc7a7c44746c77d011bc5
Jonathan J <jona.jorg@gmail.com>
2022-10-11 20:19:40 +0200

    Fix scale in renderer

--- main.py
+++ main.py
@@ -2,65 +2,35 @@ import pygame
 import numpy as np
 from perlin_noise import perlin_noise
 from world.creature import Creature
-from rendering import rendering
+from rendering import Renderer
 from world.world import World
-
 
 pygame.init()
 grid_size = 20
-cell_size = 32
-screen = pygame.display.set_mode([grid_size * cell_size, grid_size * cell_size])
-
-
-def noise_func(size):
-    noise = perlin_noise(octaves=5, seed=10)
-
-    x_cells, y_cells = size, size
-    grid = np.zeros((2, x_cells, y_cells))
-    pic = [[noise([i / x_cells, j / y_cells]) for j in range(x_cells)] for i in range(y_cells)]
-
-    for j in range(x_cells):
-        for i in range(y_cells):
-            grid[0][j][i] = abs(pic[j][i]) * 180 + 75
-
-    return grid
-
-
-def draw_grid(grid):
-    for layer in range(len(grid)):
-        for y in range(len(grid[0])):
-            for x in range(len(grid[0][0])):
-                if grid[layer][y, x] != 0 and layer == 0:
-                    pygame.draw.rect(screen, (0, grid[layer][y][x], 0),
-                                     (cell_size * x, cell_size * y, cell_size, cell_size))
-                elif grid[layer][y, x] != 0 and layer == 1:
-                    pygame.draw.circle(screen, (grid[layer][y][x], 0, 0),
-                                       (cell_size * x - cell_size / 2, cell_size * y + cell_size / 2), cell_size / 2)
-    return grid
-
+scale = 16
+screen = pygame.display.set_mode([grid_size * scale, grid_size * scale])
 
 if __name__ == '__main__':
+    renderer = Renderer(None, scale)
     pygame.init()
-    grid_size = 20
-    cell_size = 50
-    screen = pygame.display.set_mode([grid_size * cell_size, grid_size * cell_size])
+    screen = pygame.display.set_mode([grid_size * scale, grid_size * scale])
     world = World(grid_size)
     creatures = [Creature(5, 5), Creature(7, 5), Creature(5, 10)]
     clock = pygame.time.Clock()
     running = True
     while running:
         screen.fill((0, 100, 0))
-        rendering.draw_grass(world.get_grid(), screen)
+        renderer.draw_grass(world.get_grid(), screen)
         for c in creatures:
-            rendering.draw_creature(c, screen)
+            renderer.draw_creature(c, screen)
             world.eat_grass(c.y, c.x)
             c.turn()
             c.walk()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
         world.step_grass()
         pygame.display.flip()
-        clock.tick(1)
-
+        clock.tick(0)
     pygame.quit()
--- rendering.py
+++ rendering.py
@@ -2,26 +2,27 @@ import numpy as np
 import pygame
 
 layer_dict = {'grass': 0, 'earth': 1, 'water': 2, 'player': 10, 'temperature': 3, 'heigh': 4, 'sun': 5}
-cell_size = 50
 black = (0, 0, 0)
 white = (200, 200, 200)
 red = (255, 0, 0)
 
+class Renderer:
+    def __init__(self, world, scale):
+        self.scale = scale
+        self.world = world
 
-class rendering:
-    @staticmethod
-    def draw_grass(grid, screen):
+    def draw_grass(self, grid, screen):
         for y in range(len(grid[0])):
             for x in range(len(grid[0][0])):
-                pygame.draw.rect(screen, (0, grid[layer_dict['grass']][y][x] * 180 + 75, 0), (50 * x, 50 * y, 50, 50))
+                pygame.draw.rect(screen, (0, grid[layer_dict['grass']][y][x] * 180 + 75, 0), (self.scale * x, self.scale * y, self.scale, self.scale))
 
-    @staticmethod
-    def draw_player(grid, screen):
+    """
+    def draw_player(self, grid, screen):
         for y in range(len(grid[0])):
             for x in range(len(grid[0][0])):
                 if grid[layer_dict['player']][y][x] != None:
                     pygame.draw.circle(screen, (grid[layer_dict['player']][y][x], 0, 0), (50 * x - 25, 50 * y + 25), 20)
-
+    
     @staticmethod
     def draw_earth(grid, screen):
         for y in range(len(grid[0])):
@@ -57,21 +58,21 @@ class rendering:
         for y in range(len(grid[0])):
             for x in range(len(grid[0][0])):
                 pygame.draw.rect(screen, (abs((grid[layer_dict['sun']][y][x]*200)),abs(grid[layer_dict['sun']][y][x]*200),0), (50 * x, 50 * y, 50, 50))
+    """
 
-    @staticmethod
-    def draw_creature(c, screen):
+    def draw_creature(self, c, screen):
         (N, W, S, E) = [0, 1, 2, 3]
-        rect = pygame.Rect(c.x * cell_size, c.y * cell_size, cell_size, cell_size)
+        rect = pygame.Rect(c.x * self.scale, c.y * self.scale, self.scale, self.scale)
         pygame.draw.rect(screen, black, rect)
         if c.d == S:
-            pygame.draw.line(screen, red, ((c.x * cell_size) + cell_size / 2, (c.y * cell_size) + cell_size / 2),
-                             ((c.x * cell_size) + cell_size / 2, (c.y * cell_size)), 3)
+            pygame.draw.line(screen, red, ((c.x * self.scale) + self.scale / 2, (c.y * self.scale) + self.scale / 2),
+                             ((c.x * self.scale) + self.scale / 2, (c.y * self.scale)), 3)
         elif c.d == E:
-            pygame.draw.line(screen, red, ((c.x * cell_size) + cell_size / 2, (c.y * cell_size) + cell_size / 2),
-                             ((c.x * cell_size), (c.y * cell_size) + cell_size / 2), 3)
+            pygame.draw.line(screen, red, ((c.x * self.scale) + self.scale / 2, (c.y * self.scale) + self.scale / 2),
+                             ((c.x * self.scale), (c.y * self.scale) + self.scale / 2), 3)
         elif c.d == N:
-            pygame.draw.line(screen, red, ((c.x * cell_size) + cell_size / 2, (c.y * cell_size) + cell_size / 2),
-                             ((c.x * cell_size) + cell_size / 2, (c.y * cell_size) + cell_size), 3)
+            pygame.draw.line(screen, red, ((c.x * self.scale) + self.scale / 2, (c.y * self.scale) + self.scale / 2),
+                             ((c.x * self.scale) + self.scale / 2, (c.y * self.scale) + self.scale), 3)
         elif c.d == W:
-            pygame.draw.line(screen, red, ((c.x * cell_size) + cell_size / 2, (c.y * cell_size) + cell_size / 2),
-                             ((c.x * cell_size) + cell_size, (c.y * cell_size) + cell_size / 2), 3)
+            pygame.draw.line(screen, red, ((c.x * self.scale) + self.scale / 2, (c.y * self.scale) + self.scale / 2),
+                             ((c.x * self.scale) + self.scale, (c.y * self.scale) + self.scale / 2), 3)
