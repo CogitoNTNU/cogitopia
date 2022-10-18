b5cc451d10628c9b495c92e060089eeeb6e22324
albertbayazidi <albert.bayazidi@gmail.com>
2022-10-18 20:30:56 +0200

    began work on neat

---
+++ neat/nn_neat.py
@@ -0,0 +1,17 @@
+from rendering import Renderer
+from world.world import World, WorldSettings
+from world.creature import Creature
+
+Creature.EAT()  
+Creature.
+
+grid_size = 20 
+ws = WorldSettings()
+ws.grass_growth_rate = 5 # Example use of ws
+
+world = World(grid_size, ws)
+c = world.spawn_creature(5, 5)
+
+
+def train_brain(self,,config):
+
