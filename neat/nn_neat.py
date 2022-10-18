5f52d8c8669e78edadc31294eb0a1af0c4cc25bc
albertbayazidi <albert.bayazidi@gmail.com>
2022-10-18 21:01:49 +0200

    began working on eval genome

--- neat/nn_neat.py
+++ neat/nn_neat.py
@@ -1,17 +1,30 @@
-from rendering import Renderer
-from world.world import World, WorldSettings
+from world.world import World
 from world.creature import Creature
-
-Creature.EAT()  
-
-
-grid_size = 20 
-ws = WorldSettings()
-ws.grass_growth_rate = 5 # Example use of ws
-
-world = World(grid_size, ws)
-c = world.spawn_creature(5, 5)
+import os
+import neat
 
 
-def train_brain(self,c,config):
-    print("Training brain")
+#om man ønsker å bruke en tidlgerere generasjon kommenter ut p, og bruk heller load checkpoint
+def brain_initialize(config):
+    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+    
+    winner = p.run(eval_genomes, 10)
+
+def eval_genomes(genomes, config):
+    #TRENGER Å SE ENERGI NIVÅ TIL CREATURE
+    pass
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                            neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                            config_path)
+    brain_initialize()
